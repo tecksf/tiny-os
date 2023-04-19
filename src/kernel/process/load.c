757ce382612224070328a368bd21375d4eab4228
txf <xftecksf@outlook.com>
2023-04-20 00:45:21 +0800

    用户程序加载问题修复
    增加用户程序空间为2048个扇区

--- src/kernel/process/load.c
+++ src/kernel/process/load.c
@@ -7,11 +7,20 @@
 
 void load_program()
 {
-    int ret;
-    ret = ide_read_sections(2, 0, temp_user_space_start, 8);
-    if (ret != 0)
+    char *dest_address = temp_user_space_start;
+    uint32 num_sections = 4096 * 256 / 512;
+
+    int ret, section_number = 0;
+
+    while (section_number < num_sections)
     {
-        panic("fail to read user program!");
+        ret = ide_read_sections(2, section_number, dest_address, 8);
+        if (ret != 0)
+        {
+            panic("fail to read user program!");
+        }
+        section_number += 8;
+        dest_address += 4096;
     }
 
     struct ElfHeader *elf = (struct ElfHeader *) temp_user_space_start;
